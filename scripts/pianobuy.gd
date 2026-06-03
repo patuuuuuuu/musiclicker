d1ce8e166f2ecd27defafbbfed2748f45b872be4
patuuuuuuu <patudaducc@gmail.com>
2026-06-03 00:59:36 -0300

    refinamentos
    mudancas na area de posicionamento adicionando feedback
    loja mais responsiva

--- scripts/pianobuy.gd
+++ scripts/pianobuy.gd
@@ -1,8 +1,17 @@
 extends TextureButton
 
+@onready var preco_atual: Label = $preco
+
+var compravel = false
+
 func _ready() -> void:
 	$escurecer.modulate.a = 0.6
 	
 func _on_game_app_update(counter) -> void:
 	if counter >= 100:
 		$escurecer.modulate.a = 0.0
+		compravel = true
+
+func _on_pressed() -> void:
+	if compravel:
+		preco_atual.text = " -" 
